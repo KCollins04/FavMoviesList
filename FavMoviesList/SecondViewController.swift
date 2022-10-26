1ce67c527caacc44e4cd275b4213ec1e105cb169
ok <kelsey.collins23@d155.org>
2022-10-26 08:08:58 -0500

    yup

--- FavMoviesList/SecondViewController.swift
+++ FavMoviesList/SecondViewController.swift
@@ -15,10 +15,12 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var movieInfoLabel: UILabel!
     
     var mt: String!
+    var info: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         movieTitle.text = mt
+        movieInfoLabel.text = info
         // Do any additional setup after loading the view.
     }
     
--- FavMoviesList/ViewController.swift
+++ FavMoviesList/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
     @IBOutlet weak var textMovie: UITextField!
     var movies = ["Cars","Cars2","Frozen","Frozen2","Smile","Up","The Little Mermaid","Lion King","Mulan","Aladdin","Beauty and the beast ","Pinocchio","Alice in Wonderland"]
     var selectMovie = ""
-    var detail = ["Fun movies about Cars","Second movie about Cars",""]
+    var selectInfo = ""
+    var detail = ["Fun movies about Cars","Second movie about Cars","epic jurnery of winter wonderland","horror movie about","A house that can fly","Mermaid that loves perince","Movie about animals","girl name Mulan pretend to be a man","Carpet that flies","Gild who falls in love with a beast","A wooden boy who thinks he is a boy","Alice in a wonderland"]
+    
+    
+    @IBOutlet weak var imageMovie: UIImageView!
+    
+    @IBOutlet weak var cellTitle: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -30,7 +38,7 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return movies.count
     }
-    
+    //each cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableViewMovie.dequeueReusableCell(withIdentifier: "myCell")!
         cell.textLabel?.text = movies[indexPath.row]
@@ -38,17 +46,17 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let cat = tableViewMovie.cellForRow(at: indexPath)?.textLabel?.text{
-            selectMovie = cat
-            print(selectMovie)
+        selectMovie = movies[indexPath.row]
+        selectInfo = detail[indexPath.row]
             performSegue(withIdentifier: "movieSegue", sender: nil)
-        }
+        
     }
     
-    
+    //
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let seg = segue.destination as! SecondViewController
         seg.mt = selectMovie
+        seg.info = selectInfo
     }
     
     
